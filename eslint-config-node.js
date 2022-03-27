62236759d238f533a1ba58c475ba249958df755e
Bartosz Dotryw <burtekdotryw@gmail.com>
2022-03-28 00:21:09 +0200

    Added new config: node

---
+++ eslint-config-node.js
@@ -0,0 +1,27 @@
+module.exports = {
+    extends: [
+        'plugin:n/recommended',
+        'plugin:security-node/recommended',
+        './eslint-config-base.js'
+    ],
+    plugins: [
+        'node',
+        'security-node'
+    ],
+    env: {
+        node: true
+    },
+    rules: {
+        'no-console': 'off',
+
+        'n/no-path-concat': 'error',
+        'n/callback-return': 'error',
+        'n/prefer-global/buffer': 'error',
+        'n/prefer-global/console': 'error',
+        'n/prefer-global/process': 'error',
+        'n/prefer-global/url-search-params': 'error',
+        'n/prefer-global/url': 'error',
+        'n/prefer-promises/dns': 'error',
+        'n/prefer-promises/fs': 'error'
+    }
+};
