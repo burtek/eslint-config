cf5df1fff695ee599de1895faa798ea5af6e6980
Bartosz <burtekdotryw@gmail.com>
2024-01-08 23:59:53 +0100

    Fix lint

--- configs/testing-library.js
+++ configs/testing-library.js
@@ -8,17 +8,19 @@ const files = ['**/*.test.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'];
 // eslint-disable-next-line no-warning-comments
 // TODO: move to FlatConfig once testing-library and jest-dom are upgraded
 export function prepareConfig() {
-    return defineFlatConfig([{
-        files,
-        plugins: {
-            'jest-dom': { rules: jestDom.rules },
-            'testing-library': { rules: testingLibrary.rules }
-        },
-        rules: {
-            ...jestDom.configs.all.rules,
-            ...testingLibrary.configs.react.rules,
-            'testing-library/prefer-explicit-assert': 'error',
-            'testing-library/prefer-user-event': 'warn'
+    return defineFlatConfig([
+        {
+            files,
+            plugins: {
+                'jest-dom': { rules: jestDom.rules },
+                'testing-library': { rules: testingLibrary.rules }
+            },
+            rules: {
+                ...jestDom.configs.all.rules,
+                ...testingLibrary.configs.react.rules,
+                'testing-library/prefer-explicit-assert': 'error',
+                'testing-library/prefer-user-event': 'warn'
+            }
         }
-    }]);
+    ]);
 }
