7e6407d617a1f390eb2b012c358aa8b097135785
Bartosz <burtekdotryw@gmail.com>
2024-04-17 16:40:36 +0200

    feat: export subconfig factories

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 export { configs, prepareConfig } from './configs/index.js';
+export { prepareConfig as prepareCypressConfig } from './configs/cypress.js';
+export { prepareConfig as prepareJestConfig } from './configs/jest.js';
+export { prepareConfig as prepareJsonConfig } from './configs/json.js';
+export { prepareConfig as prepareLodashConfig } from './configs/lodash.js';
+export { prepareConfig as prepareNodeConfig } from './configs/node.js';
+export { prepareConfig as prepareReactConfig } from './configs/react.js';
+export { prepareConfig as prepareTestingLibraryConfig } from './configs/testing-library.js';
