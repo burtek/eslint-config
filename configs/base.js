f626227f722acc7cbc5395986843960bf9615800
Bartosz <burtekdotryw@gmail.com>
2024-06-06 21:57:00 +0200

    chore: Rename deprecated rules as per typescript-eslint changes

--- configs/base.js
+++ configs/base.js
@@ -496,7 +496,7 @@ export function prepareConfig() {
                 ],
                 '@typescript-eslint/no-useless-constructor': 'error',
                 '@typescript-eslint/no-useless-empty-export': 'error',
-                '@typescript-eslint/no-useless-template-literals': 'error',
+                '@typescript-eslint/no-unnecessary-template-expression': 'error',
                 '@typescript-eslint/no-var-requires': 'error',
                 '@typescript-eslint/only-throw-error': 'error', // extends no-throw-literal
                 '@typescript-eslint/parameter-properties': ['error', { prefer: 'parameter-property' }],
