44e95b95d17cb176bbbf30ff5a029c49a299cef0
Bartosz <burtekdotryw@gmail.com>
2024-08-22 19:28:09 +0200

    feat: Add extendFlatConfig

--- configs/testing-library.js
+++ configs/testing-library.js
@@ -2,25 +2,30 @@ import jestDom from 'eslint-plugin-jest-dom';
 import testingLibrary from 'eslint-plugin-testing-library';
 import tseslint from 'typescript-eslint';
 
+import { extendFlatConfig } from './share/extend-flat-config.js';
+
 
 const files = ['**/*.test.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'];
 
 export function prepareConfig() {
     return tseslint.config(
-        {
-            ...jestDom.configs['flat/all'],
-            name: 'dtrw:testing-lib:jest-dom',
-            files
-        },
-        {
-            ...testingLibrary.configs['flat/react'],
-            name: 'dtrw:testing-lib:react',
-            files,
-            rules: {
-                ...testingLibrary.configs['flat/react'].rules,
-                'testing-library/prefer-explicit-assert': 'error',
-                'testing-library/prefer-user-event': 'warn'
+        extendFlatConfig(
+            jestDom.configs['flat/all'],
+            {
+                name: 'dtrw:testing-lib:jest-dom',
+                files
             }
-        }
+        ),
+        extendFlatConfig(
+            testingLibrary.configs['flat/react'],
+            {
+                name: 'dtrw:testing-lib:react',
+                files,
+                rules: {
+                    'testing-library/prefer-explicit-assert': 'error',
+                    'testing-library/prefer-user-event': 'warn'
+                }
+            }
+        )
     );
 }
