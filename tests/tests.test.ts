919249a9af82037359b728dc0eee8564aa4f2ce6
Bartosz <2940958+burtek@users.noreply.github.com>
2025-07-23 14:50:42 +0200

    chore(tests): switch to vitest (#519)
    * chore(test): switch to vitest
    
    * chore(tests): ruin tests for both jest and vitest
    
    * chore(tests): fix tests

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -7,43 +7,57 @@ import tseslint from 'typescript-eslint';
 import { prepareConfig } from '../configs';
 
 
-// FIXME: doesn't work
-jest.mock('@typescript-eslint/types', () => ({
-    ...jest.requireActual('@typescript-eslint/types'),
-}))
+vitest.mock(import('@typescript-eslint/types'), async importOriginal => ({ ...await importOriginal() }));
 
-describe('Eslint configs', () => {
-    let config: Linter.Config[];
+describe.each<{
+    _: string;
+    config: Pick<NonNullable<Parameters<typeof prepareConfig>[0]>, 'jest'>;
+    settings: Partial<Record<'jest', { version: number }>>;
+}>([
+    {
+        _: 'vitest',
+        config: { jest: { mode: 'vitest' } },
+        settings: {}
+    },
+    {
+        _: 'jest',
+        config: { jest: true },
+        settings: { jest: { version: 30 } }
+    }
+])('Eslint configs in $_ mode', ({ config: conf, settings }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
+    const config = tseslint.config(
+        ...prepareConfig({
+            cypress: true,
+            jest: conf.jest,
+            json: true,
+            lodash: true,
+            node: true,
+            react: true,
+            testingLibrary: true
+        }),
+        {
+            name: 'local-overrides',
+            languageOptions: {
+                parserOptions: {
+                    disallowAutomaticSingleRunInference: true,
+                    project: './tsconfig.test.json'
+                },
+                sourceType: 'module'
+            },
+            settings: {
+                react: { version: '19.0' },
+                ...settings
+            }
+        }
+    ) as Linter.Config[];
 
     const testsDirname = resolve(process.cwd(), 'tests');
     const fixturesPath = resolve(testsDirname, 'fixtures');
     const files = readdirSync(fixturesPath, { withFileTypes: true }).filter(f => f.isFile() && f.name.startsWith('file-'));
 
-    beforeAll(() => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
-        config = tseslint.config(
-            ...prepareConfig({
-                cypress: true,
-                jest: true,
-                json: true,
-                lodash: true,
-                node: true,
-                react: true,
-                testingLibrary: true
-            }),
-            {
-                name: 'local-overrides',
-                languageOptions: {
-                    parserOptions: { project: 'tsconfig.test.json' },
-                    sourceType: 'module'
-                },
-                settings: { react: { version: '18.0' } }
-            }
-        ) as Linter.Config[];
-    });
-
-    it.each(files)('should return no errors for $name', async ({ path, name }) => {
-        const content = await fs$.readFile(resolve(path, name), { encoding: 'utf8' });
+    it.each(files)('should return no errors for $name', async ({ parentPath, name }) => {
+        const content = await fs$.readFile(resolve(parentPath, name), { encoding: 'utf8' });
 
         const linter = new Linter({ configType: 'flat' });
         let lintResult: unknown[] = [];
@@ -54,7 +68,8 @@ describe('Eslint configs', () => {
                 resolve('tests/fixtures', name)
             );
         }
+
         expect(validate).not.toThrow();
-        expect(lintResult).toHaveLength(0);
-    }, 1000);
+        expect(lintResult).toStrictEqual([]);
+    }, 2000);
 });
