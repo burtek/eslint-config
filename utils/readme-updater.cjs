b41a05237a750e47ddc586cb81188e1f94f2ca98
Bartosz <2940958+burtek@users.noreply.github.com>
2026-07-16 17:23:15 +0200

    chore: enhance readme-updater to handle version updates

--- utils/readme-updater.cjs
+++ utils/readme-updater.cjs
@@ -2,9 +2,16 @@ module.exports.readVersion = function(contents) {
     return /@dtrw\/eslint-config@~(\d+\.\d+\.\d+) eslint/.exec(contents)[1];
 }
 module.exports.writeVersion = function (contents, version) {
+    if (version.includes('-')) return contents; // do not write prerelease version to README;
+    
     const newVersion = version.split('.').with(2, 0).join('.');
-    return contents.replaceAll(
-        /@dtrw\/eslint-config@~\d+\.\d+\.\d+ eslint/g,
-        `@dtrw/eslint-config@~${newVersion} eslint`
-    );
+    return contents
+        .replaceAll(
+            /@dtrw\/eslint-config@~\d+\.\d+\.\d+ eslint/g,
+            `@dtrw/eslint-config@~${newVersion} eslint`
+        )
+        .replaceAll(
+            /badge\.socket\.dev\/npm\/package\/@dtrw\/eslint-config\/\d+\.\d+\.\d+\)/g,
+            `badge.socket.dev/npm/package/@dtrw/eslint-config/${newVersion})`
+        );
 };
